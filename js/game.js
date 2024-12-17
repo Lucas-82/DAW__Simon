dba1ef9845ca7175aeb9bd355ffe462fa3f1a1e0
lucas <lucasivan2000@hotmail.com>
2024-12-17 09:54:26 -0300

    ultimas funciones de game.js

--- js/game.js
+++ js/game.js
@@ -181,3 +181,178 @@ function createLevel() {
         break;
     }
   }
+
+  function getRanking() {
+    var storedRanking = localStorage.getItem('ranking');
+    return storedRanking ? JSON.parse(storedRanking) : [];
+  }
+
+  function updateRanking(newItem) {
+    var ranking = getRanking();
+    ranking.push(newItem);
+    localStorage.setItem('ranking', JSON.stringify(ranking));
+  }
+
+  function showFinalResults() {
+    finalPoints = currentPoints - calculatePenalization();
+    var points = 'Puntos alcanzados: ' + currentPoints;
+    var penalization = 'Penalización: ' + calculatePenalization();
+    var final = 'PUNTAJE FINAL: ' + finalPoints;
+    openModal(
+      'Ingresaste un color incorrecto! Perdiste :(\n\n' +
+        points +
+        '\n' +
+        penalization +
+        '\n\n' +
+        final
+    );
+  }
+
+  function saveResult() {
+    var result = {
+      name: nameInput.value,
+      level: currentLevel,
+      points: finalPoints,
+      date: Date(Date.now()),
+    };
+    updateRanking(result);
+  }
+
+  function showRanking(order) {
+    ranking.style.display = 'flex';
+    rankingList.innerHTML = '';
+    var storedRanking = getRanking();
+    storedRanking = storedRanking.sort(function (a, b) {
+      return b.points - a.points;
+    });
+    storedRanking = storedRanking.slice(0, 10);
+    var rankingWithPosition = storedRanking.map(function (item, index) {
+      return {
+        position: index + 1, // add position number
+        name: item.name,
+        level: item.level,
+        points: item.points,
+        date: item.date,
+      };
+    });
+    switch (order) {
+      case RankingOrder.name:
+        rankingWithPosition = rankingWithPosition.sort(function (a, b) {
+          return a.name.localeCompare(b.name);
+        });
+        break;
+      case RankingOrder.level:
+        rankingWithPosition = rankingWithPosition.sort(function (a, b) {
+          return b.level - a.level;
+        });
+        break;
+      case RankingOrder.points:
+        rankingWithPosition = rankingWithPosition.sort(function (a, b) {
+          return b.points - a.points;
+        });
+        break;
+      case RankingOrder.date:
+        rankingWithPosition = rankingWithPosition.sort(function (a, b) {
+          return new Date(b.date) - new Date(a.date);
+        });
+        break;
+    }
+    while (rankingWithPosition.length < 10) {
+      rankingWithPosition.push({
+        name: '-',
+        level: '-',
+        points: '-',
+        date: '-',
+        position: 10,
+      });
+    }
+    rankingWithPosition.forEach(function (item) {
+      var rankingItem = document.createElement('li');
+      rankingItem.classList.add('ranking-item');
+      rankingItem.innerHTML =
+        createSpanRankingItem('# ' + item.position) +
+        createSpanRankingItem(item.name.toUpperCase()) +
+        createSpanRankingItem(item.level) +
+        createSpanRankingItem(item.points) +
+        createSpanRankingItem(visualFormattedDatetime(item.date));
+      rankingList.appendChild(rankingItem);
+    });
+  }
+
+  function createSpanRankingItem(value) {
+    return '<span>' + value + '</span>';
+  }
+
+  function clearGame() {
+    gameStarted = false;
+    currentLevel = 0;
+    currentPoints = 0;
+    finalPoints = 0;
+    sequence = [];
+    nameInput.disabled = false;
+    clearSequenceEntered();
+    setGameState(PlayingStatus.notStarted);
+    setStartButton('INICIAR');
+    showLevel(0);
+    showPoints(0);
+    clearTimer();
+  }
+
+  function showLevel(value) {
+    labelLevel.innerText = 'Nvl: ' + value;
+  }
+
+  function showPoints(value) {
+    labelScore.innerText = 'Pts: ' + value;
+  }
+
+  function showTimer(value) {
+    labelTimer.innerText = value;
+  }
+
+  function clearSequenceEntered() {
+    sequenceEntered = [];
+  }
+  function setGameState(newState) {
+    labelState.innerText = newState;
+    switch (newState) {
+      case PlayingStatus.notStarted:
+        labelState.style.color = 'white';
+        break;
+      case PlayingStatus.sequenceShowing:
+        labelState.style.color = 'yellow';
+        break;
+      case PlayingStatus.sequenceWaiting:
+        labelState.style.color = 'tomato';
+        break;
+      case PlayingStatus.sequenceCorrect:
+        labelState.style.color = 'lightgreen';
+        break;
+    }
+  }
+
+  function setStartButton(title) {
+    buttonStart.innerText = title;
+  }
+
+  /* utils */
+  function to2Places(value) {
+    return String(value).padStart(2, '0');
+  }
+
+  function calculatePenalization() {
+    return Math.trunc((hours * 60 * 60 + minutes * 60 + seconds) / 15);
+  }
+  
+  function visualFormattedDatetime(datetime) {
+    var parsedDate = new Date(datetime);
+    if (isNaN(parsedDate)) {
+      return '-';
+    }
+    var day = to2Places(parsedDate.getDate());
+    var month = to2Places(parsedDate.getMonth() + 1);
+    var year = parsedDate.getFullYear();
+    var hours = to2Places(parsedDate.getHours());
+    var minutes = to2Places(parsedDate.getMinutes());
+    return day + '/' + month + '/' + year + ' ' + hours + ':' + minutes;
+  }
